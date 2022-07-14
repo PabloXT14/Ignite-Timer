f4896260548b7072de9f5c790cd324a043bbaae3
Pablo Alan <pabloxt14@gmail.com>
2022-07-14 13:21:28 -0300

    feat: fix problems with eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button bgColor="primary">Send</Button>
-      <Button bgColor="secondary">Send</Button>
-      <Button bgColor="danger">Send</Button>
-      <Button bgColor="success">Send</Button>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/Router.tsx
@@ -0,0 +1,7 @@
+import { Routes, Route } from 'react-router-dom';
+
+export function Router() {
+    return (
+        
+    );
+}
