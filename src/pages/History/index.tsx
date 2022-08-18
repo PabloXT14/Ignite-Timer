82630d1251f79daaf872acd4d952d00e0ccabfda
Pablo Alan <pabloxt14@gmail.com>
2022-08-18 17:35:18 -0300

    feat: using cycles context in History page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,16 +1,15 @@
 import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { CyclesContext } from '../../contexts/CyclesContext'
 import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
-  const arrayTest = [1, 2, 3, 4, 5, 6, 7]
   const { cycles } = useContext(CyclesContext)
 
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
-
-      <pre>{JSON.stringify(cycles, null, 2)}</pre>
 
       <HistoryList>
         <table>
@@ -23,14 +22,29 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            {arrayTest.map((element) => {
+            {cycles.map((cycle) => {
               return (
-                <tr key={element}>
-                  <td>Tarefa</td>
-                  <td>20 minutos</td>
-                  <td>Há 2 meses</td>
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
                   <td>
-                    <Status statusColor="green">Concluído</Status>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
+                  <td>
+                    {cycle.finishedDate && (
+                      <Status statusColor="green">Concluído</Status>
+                    )}
+
+                    {cycle.interruptedDate && (
+                      <Status statusColor="red">Interrompido</Status>
+                    )}
+
+                    {!cycle.finishedDate && !cycle.interruptedDate && (
+                      <Status statusColor="yellow">Em andamento</Status>
+                    )}
                   </td>
                 </tr>
               )
